e01e52aec8bf016a857ba4ce5c59de007251cca2
sshivaji <sshivaji@cs.odu.edu>
2021-12-14 13:28:44 -0500

    first commit

--- Connect4.py
+++ Connect4.py
@@ -3,31 +3,39 @@ import random
 import pygame
 import sys
 import math
+import time
 
+# Colours  defined for the pygame
 BLUE = (0,0,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 YELLOW = (255,255,0)
 
+# Initialisation values for the rows and columns
 ROW_COUNT = 6
 COLUMN_COUNT = 7
 
 PLAYER = 0
 AI = 1
 
+# Programatic depiction of the players and the empty places
 EMPTY = 0
 PLAYER_PIECE = 1
 AI_PIECE = 2
 
+# Number of dots to be connected to win the game
 WINDOW_LENGTH = 4
 
+# Initialisation of the board with all 0's(Empty places)
 def create_board():
 	board = np.zeros((ROW_COUNT,COLUMN_COUNT))
 	return board
 
+# Function to add a disc/pice to a specific column
 def drop_piece(board, row, col, piece):
 	board[row][col] = piece
 
+# Checks if the move is valid, returns False if the column is already full
 def is_valid_location(board, col):
 	return board[ROW_COUNT-1][col] == 0
 
@@ -36,9 +44,11 @@ def get_next_open_row(board, col):
 		if board[r][col] == 0:
 			return r
 
+# Show the board as the program sees it in the program running window (Used for debugging the issues)
 def print_board(board):
 	print(np.flip(board, 0))
 
+# Check if the last made move was a winning move in all the directions
 def winning_move(board, piece):
 	# Check horizontal locations for win
 	for c in range(COLUMN_COUNT-3):
@@ -64,6 +74,7 @@ def winning_move(board, piece):
 			if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
 				return True
 
+# Evaluation function
 def evaluate_window(window, piece):
 	score = 0
 	opp_piece = PLAYER_PIECE
@@ -82,9 +93,9 @@ def evaluate_window(window, piece):
 
 	return score
 
+# Generate score for a board configuration
 def score_position(board, piece):
 	score = 0
-
 	center_array = [int(i) for i in list(board[:, COLUMN_COUNT//2])]
 	center_count = center_array.count(piece)
 	score += center_count * 3
@@ -113,10 +124,12 @@ def score_position(board, piece):
 
 	return score
 
+# Check if the game is ending
 def is_terminal_node(board):
 	return winning_move(board, PLAYER_PIECE) or winning_move(board, AI_PIECE) or len(get_valid_locations(board)) == 0
 
-def minimax(board, depth, alpha, beta, maximizingPlayer):
+# Min max with alpha beta pruning algorithm implementation
+def minimaxwithabpruning(board, depth, alpha, beta, maximizingPlayer):
 	valid_locations = get_valid_locations(board)
 	is_terminal = is_terminal_node(board)
 	if depth == 0 or is_terminal:
@@ -136,7 +149,7 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
 			row = get_next_open_row(board, col)
 			b_copy = board.copy()
 			drop_piece(b_copy, row, col, AI_PIECE)
-			new_score = minimax(b_copy, depth-1, alpha, beta, False)[1]
+			new_score = minimaxwithabpruning(b_copy, depth-1, alpha, beta, False)[1]
 			if new_score > value:
 				value = new_score
 				column = col
@@ -152,7 +165,7 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
 			row = get_next_open_row(board, col)
 			b_copy = board.copy()
 			drop_piece(b_copy, row, col, PLAYER_PIECE)
-			new_score = minimax(b_copy, depth-1, alpha, beta, True)[1]
+			new_score = minimaxwithabpruning(b_copy, depth-1, alpha, beta, True)[1]
 			if new_score < value:
 				value = new_score
 				column = col
@@ -161,6 +174,7 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
 				break
 		return column, value
 
+# For the AI player to get all the valid locations in the game where the disc can be put
 def get_valid_locations(board):
 	valid_locations = []
 	for col in range(COLUMN_COUNT):
@@ -168,22 +182,23 @@ def get_valid_locations(board):
 			valid_locations.append(col)
 	return valid_locations
 
-def pick_best_move(board, piece):
-
+# MiniMax Implementation
+def minimax(board):
 	valid_locations = get_valid_locations(board)
-	best_score = -10000
+	best_score = -10000000000000
 	best_col = random.choice(valid_locations)
 	for col in valid_locations:
 		row = get_next_open_row(board, col)
 		temp_board = board.copy()
-		drop_piece(temp_board, row, col, piece)
-		score = score_position(temp_board, piece)
+		drop_piece(temp_board, row, col, AI_PIECE)
+		score = score_position(temp_board, AI_PIECE)
 		if score > best_score:
 			best_score = score
 			best_col = col
 
 	return best_col
 
+# Show the UI Created for the game using pygame
 def draw_board(board):
 	for c in range(COLUMN_COUNT):
 		for r in range(ROW_COUNT):
@@ -197,6 +212,9 @@ def draw_board(board):
 			elif board[r][c] == AI_PIECE: 
 				pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
 	pygame.display.update()
+
+
+withPruning = True if input('Use alpha-beta pruning ?(y/n)').lower() == 'y' else False
 
 board = create_board()
 print_board(board)
@@ -219,14 +237,18 @@ pygame.display.update()
 
 myfont = pygame.font.SysFont("monospace", 75)
 
-turn = random.randint(PLAYER, AI)
+turn = PLAYER
 
+timeTaken = []
+
+# Loop for the game play
 while not game_over:
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
 
+		# Checks if there is any motion and moves the red discs
 		if event.type == pygame.MOUSEMOTION:
 			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
 			posx = event.pos[0]
@@ -235,6 +257,7 @@ while not game_over:
 
 		pygame.display.update()
 
+		# Drops  the red disc into a column
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
 			if turn == PLAYER:
@@ -253,13 +276,22 @@ while not game_over:
 					turn += 1
 					turn = turn % 2
 
-					print_board(board)
+					# print_board(board)
 					draw_board(board)
 
 
 	if turn == AI and not game_over:				
 
-		col, minimax_score = minimax(board, 5, -math.inf, math.inf, True)
+		if withPruning:
+			startTime = time.time()
+			col, minimax_score = minimaxwithabpruning(board, 5, -math.inf, math.inf, True)
+			timeTaken.append(round(time.time() - startTime, 3))
+			print("Time taken with ab pruning - " + str(round(time.time() - startTime, 3)) + " seconds")
+		else:
+			startTime = time.time()
+			col = minimax(board)
+			timeTaken.append(round(time.time() - startTime, 3))
+			print("Time taken without ab pruning - " + str(round(time.time() - startTime, 3)) + " seconds")
 
 		if is_valid_location(board, col):
 
@@ -271,11 +303,12 @@ while not game_over:
 				screen.blit(label, (40,10))
 				game_over = True
 
-			print_board(board)
+			# print_board(board)
 			draw_board(board)
 
 			turn += 1
 			turn = turn % 2
 
-	if game_over:
+	if game_over: 
+		print("Average time taken - " + str(round(sum(timeTaken)/len(timeTaken), 3)) + " seconds")
 		pygame.time.wait(3000)
--- Readme.md
+++ Readme.md
@@ -12,7 +12,7 @@ python3 Connect4.py
 
 ### Details 
 
-By default the AI(First Player) will start playing the game, the red discs are for the human player. The game is made in such a way that the UI tracks the movement of the cursor and the Red plate moves on the top based on the movememnt of the cursor. On mouse left click the cursor drops the disc inside the column.
+By default the Human player will start playing the game, the red discs are for the human player. The game is made in such a way that the UI tracks the movement of the cursor and the Red plate moves on the top based on the movememnt of the cursor. On mouse left click the cursor drops the disc inside the column.
 
 * 0's are used to depict the empty places.
 * 1's are used to depict the human player discs.
@@ -22,3 +22,23 @@ By default the AI(First Player) will start playing the game, the red discs are f
 
 *  `ctrl+c` can be used to close the game.
 * The red cross can also be used to do the same.
+* Once the game ends the game closes automatically in 3 seconds.
+
+### Background prints
+
+* Time taken for each move will be printed in the background.
+* Once the game ends the average time taken will be shown.
+
+### Sample output
+```
+Time taken with ab pruning - 0.621 seconds
+Time taken with ab pruning - 0.701 seconds
+Time taken with ab pruning - 0.579 seconds
+Time taken with ab pruning - 0.416 seconds
+Time taken with ab pruning - 0.797 seconds
+Time taken with ab pruning - 0.841 seconds
+Time taken with ab pruning - 0.392 seconds
+Time taken with ab pruning - 0.833 seconds
+Time taken with ab pruning - 0.409 seconds
+Average time taken - 0.621 seconds
+```
